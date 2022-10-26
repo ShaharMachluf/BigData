7bd9b766ba8bc2d62866f5573b924b2e1099f618
chenshtynmetz <93136824+chenshtynmetz@users.noreply.github.com>
2022-10-26 15:54:19 +0300

    Update bdika_sql.js

--- bdika_sql.js
+++ bdika_sql.js
@@ -2,15 +2,14 @@ var mysql = require('mysql');
 const myobject = require("./order");
 const cities = [];
 const flavors = ["Chocolate", "Lemon", "Vanilla", "Strawberry", "Halva", "Chocolate"]
-const seasons = [1, 2, 2, 2]
+const seasons = [1, 2, 2, 2] //in the summer eat more ice cream
 const winter = [0, 1, 2, 9, 10, 11]
 const summer = [3, 4, 5, 6, 7, 8]
 const uuid = require("uuid");
 const Kafka = require("node-rdkafka");
 const kafkaApp = require('./kafkaApp');
-// const {mont} = require("./partB/dist/dashboard.975ef6c8");
 
-
+//connect to mysql
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -20,6 +19,7 @@ var con = mysql.createConnection({
 
 con.connect(function (err) {
     if (err) throw err;
+    //get the cities from the database
     con.query("SELECT name FROM cities", function (err, result, fields) {
         if (err) throw err;
         //Return the fields object:
@@ -31,6 +31,7 @@ con.connect(function (err) {
     });
 });
 
+//sleep function
 function sleep(milliseconds) {
     const date = Date.now();
     let currentDate = null;
@@ -39,14 +40,15 @@ function sleep(milliseconds) {
     } while (currentDate - date < milliseconds);
 }
 
+
 function make_orders() {
-    var r_scoops = Math.floor(Math.random() * 3) + 1;
+    var r_scoops = Math.floor(Math.random() * 3) + 1; //generate num of scoops
     var myarray = [];
-    var r_season = Math.floor(Math.random() * 3);
-    var r_city = Math.floor(Math.random() * 94); //check it
-    var r_year = Math.floor(Math.random() * 5) + 2018; //check the years
-    var r_day = Math.floor(Math.random() * 31);
-    var r_month = Math.floor(Math.random() * 5);
+    var r_season = Math.floor(Math.random() * 3); //generate season
+    var r_city = Math.floor(Math.random() * 94); //generate city
+    var r_year = Math.floor(Math.random() * 5) + 2018; //generate year
+    var r_day = Math.floor(Math.random() * 31); //generate day
+    var r_month = Math.floor(Math.random() * 5); //generate month in season
     var month = -1;
     if (seasons[r_season] === 1) {
         month = winter[r_month];
@@ -54,6 +56,7 @@ function make_orders() {
         month = summer[r_month];
     }
     var date = new Date(r_year, month, r_day);
+    //generate flavor
     if (date.getDay() >= 4) { //in weekend buy more Chocolate
         for (var i = 0; i < r_scoops; i++) {
             var r_flavor = Math.floor(Math.random() * 6);
@@ -67,27 +70,25 @@ function make_orders() {
     }
     let my_order = new myobject.Order(myarray, date, cities[r_city], r_scoops);
     let str = my_order.toString();
-    // var str_json= {"flav":myarray,"date":date, "city":cities[r_city],"r_scoops":r_scoops};
     console.log(str)
+    //send through kafka
     kafkaApp.publish(my_order);
 }
 
 
-function bdika1(){
+function firsts_order(){
     for (var i = 0; i < 200; i++) {
         make_orders();
-        // producer.connect();
-        // setTimeout(kafk.publish(str),1000);
     }
 }
 
 
-function bdika() {
+function send_orders() {
     while (true) {
         make_orders();
         sleep(1000);
     }
 }
-setTimeout(bdika1, 1000)
-setTimeout(bdika, 1000)
+setTimeout(firsts_order, 1000)
+setTimeout(send_orders, 1000)
 

51936dfbb121e93a92700f06785de69acf3f7169
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-10-27 10:50:10 +0300

    Delete sql_city.js

--- partB/src/sql_city.js
+++
@@ -1,9 +0,0 @@
-const redis = require('redis');
-const REEDIS_PORT = process.env.PORT || 6379;
-const client = redis.createClient('127.0.0.1', REEDIS_PORT);
-module.exports.dataBySnif = async function (snif, flav) {
-    var amount = await client.HGET(snif, flav);
-    return amount;
-}
-
-client.connect();
